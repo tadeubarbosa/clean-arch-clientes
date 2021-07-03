1f2466ed08b605ed5de48166e46bd8ba66a0c99e
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2021-07-03 15:39:56 -0300

    refactor: altera parametros da classe CadastrarClienteRepository

--- src/Domain/Repositories/Clientes/CadastrarClienteRepository.php
+++ src/Domain/Repositories/Clientes/CadastrarClienteRepository.php
@@ -4,18 +4,15 @@ namespace App\Domain\Repositories\Clientes;
 
 use App\Domain\Exceptions\Clientes\ClienteComEmailJaExistenteException;
 use App\Domain\Inputs\Clientes\CadastrarClienteOutputData;
-use App\Domain\ValueObjects\Shared\Email;
-use App\Domain\ValueObjects\Shared\Nome;
-use App\Domain\ValueObjects\Shared\Password;
 
 interface CadastrarClienteRepository
 {
     /**
-     * @param Nome $nome
-     * @param Email $email
-     * @param Password $password
+     * @param string $nome
+     * @param string $email
+     * @param string $password
      * @return CadastrarClienteOutputData|null
      * @throws ClienteComEmailJaExistenteException
      */
-    public function execute(Nome $nome, Email $email, Password $password): ?CadastrarClienteOutputData;
+    public function execute(string $nome, string $email, string $password): ?CadastrarClienteOutputData;
 }
